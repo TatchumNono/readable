2acbedeb56734d122838ab0f9d470bf95beafbdd
Tatchum Nono Yann <tatchumnono@gmail.com>
2021-04-06 15:14:55 +0100

    added a modal component for confirms before deletion of post

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,79 +1,96 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchPosts, fetchCategories, deletePost } from '../actions/action';
 import '../App.css';
+import './modal.css';
 import { v4 as uuidv4 } from 'uuid';
 import { useHistory } from 'react-router-dom';
+//import Modal from './Modal';
 
 const Home = () => {
+  //const modal = useRef(null);
   const dispatch = useDispatch();
   const history = useHistory();
   const posts = useSelector((state) => state.categories.posts);
   const categories = useSelector((state) => state.categories.categories);
+  //const [confirm, setConfirm] = useState(false);
+  //const [ID, setID] = useState('');
 
   useEffect(() => {
     dispatch(fetchPosts());
     dispatch(fetchCategories());
   }, [dispatch]);
 
+  const Delete = (id) => {
+    dispatch(deletePost(id));
+  };
+
   console.log(posts);
 
   return (
-    <div className="container">
-      <div>
-        <button onClick={() => history.push('/create')}>New Post</button>
-      </div>
-      <div>
-        {categories == null ? (
-          <p>No categories</p>
-        ) : (
-          <div className="categories">
-            {Object.keys(categories).map((key) =>
-              categories[key].map((item) => (
-                <button
-                  key={uuidv4()}
-                  onClick={() => history.push(`/${item.name}`)}>
-                  {item.name}
-                </button>
-              ))
-            )}
-          </div>
-        )}
+    <>
+      <div className="container">
+        <div>
+          <button onClick={() => history.push('/create')}>New Post</button>
+        </div>
+        <div>
+          {categories == null ? (
+            <p>No categories</p>
+          ) : (
+            <div className="categories">
+              {Object.keys(categories).map((key) =>
+                categories[key].map((item) => (
+                  <button
+                    key={uuidv4()}
+                    onClick={() => history.push(`/${item.name}`)}>
+                    {item.name}
+                  </button>
+                ))
+              )}
+            </div>
+          )}
 
-        {posts == null ? (
-          <p>No posts yet</p>
-        ) : (
-          posts.map((post) => (
-            <>
-              <div key={post.id} className="card">
-                <div className="header">
-                  <h3>{post.author}</h3>
-                  <p>{new Date(post.timestamp).toUTCString()}</p>
-                  <p>{post.category}</p>
+          {posts == null ? (
+            <p>No posts yet</p>
+          ) : (
+            posts.map((post) => (
+              <>
+                <div key={post.id} className="card">
+                  <div className="header">
+                    <h3>{post.author}</h3>
+                    <p>{new Date(post.timestamp).toUTCString()}</p>
+                    <p>{post.category}</p>
+                  </div>
+
+                  <h2>
+                    {/*<Link to={`/post/${post.id}`} />*/}
+                    <a href={`/${post.category}/${post.id}`}>{post.title}</a>
+                  </h2>
+
+                  <p>{post.body}</p>
+                  <div className="bottom">
+                    <p>{post.commentCount} comments</p>
+                    <p>{post.voteScore} votes</p>
+                    <button onClick={() => Delete(post.id)}>Delete</button>
+                    {/*  <button onClick={() => modal.current.open()}>Delete</button> */}
+                  </div>
                 </div>
 
-                <h2>
-                  {/*<Link to={`/post/${post.id}`} />*/}
-                  <a href={`/${post.category}/${post.id}`}>{post.title}</a>
-                </h2>
-
-                <p>{post.body}</p>
-                <div className="bottom">
-                  <button>{post.commentCount} comments</button>
-                  <p>{post.voteScore} votes</p>
-                  <button onClick={() => dispatch(deletePost(post.id))}>
+                {/* <Modal ref={modal}>
+                  <p>Confirm!</p>
+                  <button onClick={(() => setConfirm(true), Delete())}>
                     Delete
                   </button>
-                </div>
-              </div>
-            </>
-          ))
-        )}
+                </Modal> */}
+              </>
+            ))
+          )}
+        </div>
+        <div>
+          <p></p>
+        </div>
       </div>
-      <div>
-        <p></p>
-      </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/Modal.js
@@ -0,0 +1,44 @@
+import { createPortal } from 'react-dom';
+import {
+  useState,
+  useEffect,
+  useCallback,
+  useImperativeHandle,
+  forwardRef,
+} from 'react';
+import './modal.css';
+
+const modalElement = document.getElementById('modal-root');
+
+/*the 'children' key word is the props recieved from the parent component
+It is destructred directly in the modal function and used just by writing children instead of props.children
+*/
+const Modal = ({ children, defaultOpened = false }, ref) => {
+  const [isOpen, setIsOpen] = useState(defaultOpened);
+
+  //this hook is used tomake the states accessible to the parents components when using useRef
+  useImperativeHandle(
+    ref,
+    () => ({
+      open: () => setIsOpen(true),
+      close: () => setIsOpen(false),
+    }),
+    []
+  );
+
+  const handleEscape = useCallback((event) => {
+    if (event.keyCode === 27) setIsOpen(false);
+  }, []);
+
+  useEffect(() => {
+    if (isOpen) document.addEventListener('keydown', handleEscape, false);
+    return () => document.removeEventListener('keydown', handleEscape, false);
+  }, [handleEscape, isOpen]);
+
+  return createPortal(
+    isOpen ? <div className="modal">{children}</div> : null,
+    modalElement
+  );
+};
+//forwardRef is used to forward the reference to the Modal component.
+export default forwardRef(Modal);
